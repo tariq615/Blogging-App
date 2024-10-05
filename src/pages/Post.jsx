362a9536172cf88c7764c23b503b71601bb51231
tariq615 <iqbaltariq615@gmail.com>
2024-10-05 22:07:26 +0300

    finished02

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -53,7 +53,7 @@ export default function Post() {
     }
 
     return post ? (
-        <div className=" mt-24 border">
+        <div className=" mt-24 ">
             <div className="max-w-screen-lg  mx-auto p-5 sm:p-10 md:p-16 ">
           <div className="mb-10 rounded overflow-hidden flex flex-col mx-auto">
             <Link
