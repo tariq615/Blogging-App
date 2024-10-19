74bd7574f93799390c46740ac887929fb27da02f
tariq615 <iqbaltariq615@gmail.com>
2024-10-19 20:55:19 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ function App() {
   </div>) : (
         <div className="flex items-center justify-center h-screen">
             <div className="relative flex flex-col items-center">
-                <img src="../../public/images/self/mylogo.webp" alt="Loading" className="w-32 h-32 animate-spin" />
+                <img src="/images/self/mylogo.webp" alt="Loading" className="w-32 h-32 animate-spin" />
                 <h1 className="-mt-4 text-xl font-bold text-gray-700">Loading...</h1>
             </div>
         </div>
